1e5699616c06b39339bf264160ecea1a6d51c5c4
reihanramadhana <ramadhanaa44@gmail.com>
2024-03-21 11:10:51 +0700

    menambahkan class SimpleDataBas

--- modul5_1302220048/Program.cs
+++ modul5_1302220048/Program.cs
@@ -1,7 +1,50 @@
-﻿internal class Program
+﻿public class SimpleDataBase<T>
 {
-    private static void Main(string[] args)
+    private List<T> storeData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
     {
-        Console.WriteLine("Hello, World!");
+        this.storeData = new List<T>();
+        this.inputDates = new List<DateTime>();
+
+    }
+
+    public void AddNewData(T data)
+    {
+        this.storeData.Add(data);
+        this.inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        int i = 0;
+        this.storeData.ForEach(data =>
+        {
+            i++;
+            Console.WriteLine("Data " + i + " berisi: " + data + ", yang disimpan pada waktu UTC: " + this.inputDates[this.storeData.IndexOf(data)]);
+        });
     }
 }
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+           SimpleDataBase<int> data = new SimpleDataBase<int>();
+           data.AddNewData(12);
+           data.AddNewData(02);
+           data.AddNewData(22);
+
+           data.PrintAllData();
+
+        }
+
+    }
+
+
+
+
+
+
+   
+    
