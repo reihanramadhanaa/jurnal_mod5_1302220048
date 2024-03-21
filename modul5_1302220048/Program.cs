39b5ad2047c97e593bbb4217e14e663889d6de3d
reihanramadhana <ramadhanaa44@gmail.com>
2024-03-21 10:23:28 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- modul5_1302220048/Program.cs
+++ modul5_1302220048/Program.cs
@@ -1,7 +1,23 @@
-﻿internal class Program
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Penjumlahan
+{
+    public static T JumlahTigaAngka<T>(T data1,T data2,T datta3) 
+    { 
+       
+
+       return (dynamic)data1 + (dynamic)data2 + (dynamic)datta3;
+    
+    }
+
+
+}
+
+class Program 
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(Penjumlahan.JumlahTigaAngka<int>(13, 02, 00));
+        
     }
 }
